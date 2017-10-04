7ab939fb17dd7951e30a23480c1135b7206c40bd
Pedro Freme <pfreme@gmail.com>
2017-10-04 08:33:23 -0300

    Codigo comentado e documentado vai que é tua

--- bNavalRedes.c
+++ bNavalRedes.c
@@ -14,26 +14,60 @@ typedef int bool;
 #define LOCAL_SERVER_PORT 8000
 #define REMOTE_SERVER_PORT 8000
 #define MAX_MSG 100
-
 #define TSIZE 10
 
+/**
+ * Struct para assim termos apenas uma matriz para trabalhar
+ */
 typedef struct sCasa{
-
 	int id;
 	char c;
-
 } casa;
 
+/**
+ * Opções da funcao imprime campo, mas acho que não vamos usar
+ */
 enum Print_Opc{
 	CLARO = 0,
 	ESCURO,
 	IDS
 };
 
+/**
+ * Inicializa o campo passado com todos ids 0 e caracteres .
+ * @param T Campo a ser inicalizado
+ */
 void inicializa(casa T[TSIZE][TSIZE]);
+
+/**
+ * Printa na tela o campo requisitado
+ * @param T   Campo a ser printado
+ * @param opt opções para campo claro, escuro, e ids para debug
+ */
 void imprimeCampo(casa T[TSIZE][TSIZE], int opt);
+
+/**
+ * Confere se a posiÇão selecionada é valida
+ * @param  T Campo a ser populado
+ * @param  i Coluna
+ * @param  j Linha
+ * @return   true se posição valida.
+ */
 bool confere(casa T[TSIZE][TSIZE], int i, int j);
+
+/**
+ * Responsavel por popular os barcos aka. inicialização do jogo
+ * @param T Tabuleiro a ser populado
+ */
 void insereBarcos(casa T[TSIZE][TSIZE]);
+
+/**
+ * Confere se o tiro dado acertou algum navio
+ * @param  T Campo a ser verificado
+ * @param  i Coluna?
+ * @param  j Linha?
+ * @return   1 se acertou, 2 se já havia atirado no mesmo pondo, 0 se agua
+ */
 int tiro(casa T[TSIZE][TSIZE], char i, char j);
 
 int main(){
@@ -41,6 +75,10 @@ int main(){
     int Acertos = 0;
     int servSock, sd, rc, n, cliLen, i, Linha, Coluna, shot=-1;
   	struct sockaddr_in cliAddr, servAddr, remoteServAddr;
+  	/**
+  	 * a mensagem é assim montada assim:
+  	 * s%d%dr%d < Linha, Coluna, Resposta do tiro vide tiro()
+  	 */
   	char msg[MAX_MSG];
   	struct hostent *h;
 
@@ -48,61 +86,90 @@ int main(){
 
     printf("Selecione a opcao de funcionamento:\n \t 1 - Server \n\t 2 - Client\n\n_");
     scanf("%d", &kind);
-
 	inicializa(Tabuleiro);
   	inicializa(Tabuleiro2);
 	imprimeCampo(Tabuleiro, 0);
 	insereBarcos(Tabuleiro);
-    if(kind == 1){//server stuff
 
-		  servSock=socket(AF_INET, SOCK_DGRAM, 0);
-		  if(servSock<0) {
-		    printf("%s: cannot open socket \n","argv[0]");
-		  //  exit(0);
-		  }
+    if(kind == 1){
+    	/**
+    	 * Trabalhando como server
+    	 */
+    	/**
+    	 * Cria socket do servidor
+    	 */
+		servSock=socket(AF_INET, SOCK_DGRAM, 0);
+		if(servSock<0) {
+			printf("%s: cannot open socket \n","argv[0]");
+			//  exit(0);
+		}
 
-		  /* bind local server port */
-		  servAddr.sin_family = AF_INET;
-		  servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-		  servAddr.sin_port = htons(LOCAL_SERVER_PORT);
-		  rc = bind (servSock, (struct sockaddr *) &servAddr, sizeof(servAddr));
-		  if(rc<0) {
-		    printf("%s: cannot bind port number %d \n",
-			   "argv[0]", LOCAL_SERVER_PORT);
+		/**
+		 * Popula struct do endereço do servidor e vincula socket e endereço
+		 */
+		servAddr.sin_family = AF_INET;
+		servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+		servAddr.sin_port = htons(LOCAL_SERVER_PORT);
+		rc = bind (servSock, (struct sockaddr *) &servAddr, sizeof(servAddr));
+		if(rc<0) {
+		printf("%s: cannot bind port number %d \n",
+		   "argv[0]", LOCAL_SERVER_PORT);
 		//    exit(0);
-		  }
+		}
 
-		  printf("%s: waiting for data on port UDP %u\n",
-			   "argv[0]",LOCAL_SERVER_PORT);
+		/**
+		 * Agora que o vinculo foi feito aguarda receber mensagem do client
+		 * Não sei se a ordem importa, mas o client envia primeiro a mensagem
+		 * quanto o server aguarda
+		 */
+		printf("%s: Aguardando dados do Cliente UDP %u\n",
+				"argv[0]",LOCAL_SERVER_PORT);
 
-		  /* server infinite loop */
-		  while(1) {
+		/**
+		* Agora entramos em loop infinito para receber dados continuamente do
+		* client
+		*/
+		while(1) {
 
-		    /* init buffer */
-		    memset(msg,0x0,MAX_MSG);
+			/**
+			 * Inicializo o buffer
+			 * TODO: Acredito que o MAX_MSG seja suficiente para o bNaval
+			 * se não for eh so trocar a definição.
+			 */
+			memset(msg,0x0,MAX_MSG);
 
-		    /* receive message */
-		    cliLen = sizeof(cliAddr);
-		    n = recvfrom(servSock, msg, MAX_MSG, 0,
+			/**
+			 * Aqui ele vai receber dados do cliente
+			 * Salvo o tamanho do cliente em uma variavel e passo o endereço dela,
+			 * provavelmente o recvfrom faz a mágica de calcular o tamanho de
+			 * retorno do endereço
+			 */
+			cliLen = sizeof(cliAddr);
+			n = recvfrom(servSock, msg, MAX_MSG, 0,
 				 (struct sockaddr *) &cliAddr, &cliLen);
 
-		    if(n<0) {
-		      printf("%s: cannot receive data \n","argv[0]");
-		      continue;
-		    }
+			if(n<0) {
+			  printf("%s: cannot receive data \n","argv[0]");
+			  continue;
+			}
 
-		    /* print received message */
-		    printf("%s: from %s:UDP%u : %s \n",
-			   "argv[0]",inet_ntoa(cliAddr.sin_addr),
-			   ntohs(cliAddr.sin_port),msg);
+			// /* print received message */
+			// printf("%s: from %s:UDP%u : %s \n",
+			//    "argv[0]",inet_ntoa(cliAddr.sin_addr),
+			//    ntohs(cliAddr.sin_port),msg);
 
-		    if(msg[0] == 's'){
-		    	printf("Verificando o tiro\n");
-		    	shot = tiro(Tabuleiro, msg[1], msg[2]);
-		    }
+			if(msg[0] == 's'){
+				printf("Verificando o tiro\n");
+				shot = tiro(Tabuleiro, msg[1], msg[2]);
+			}
 
-		    if(strlen(msg)>3 && msg[3] == 'r'){
-			    if(msg[4] == '1'){ //Caso a linha e a coluna batam com a # no tabuleiro verdadeiro do jogador 2, sera efetuado e mostrado o X em ambos os tabuleiros
+			if(strlen(msg)>3 && msg[3] == 'r'){
+				/**
+				 * Caso a linha e a coluna batam com a # no tabuleiro verdadeiro
+				 * do jogador 2, sera efetuado e mostrado o X em ambos os
+				 * tabuleiros
+				 */
+			    if(msg[4] == '1'){
 					Tabuleiro2[Linha][Coluna].c = 'X';
 					printf("\nVoce acertou!\n");
 					Acertos++; //o numero de acertos sobe
@@ -110,21 +177,32 @@ int main(){
 				else if(msg[4] == '2'){
 					printf("\nVoce ja atirou nessa Posicao anteriormente.\n");
 				}
-				else{ //Caso contrario sera mostrado um "." no tabuleiro de visao do jogador 1 e um ! no tabuleiro verdadeiro do jogador 2
+				/**
+				 * Caso contrario sera mostrado um "." no tabuleiro de visao
+				 * do jogador 1 e um ! no tabuleiro verdadeiro do jogador 2
+				 */
+				else{
+
 					Tabuleiro2[Linha][Coluna].c = '.';
 					printf("\nVoce errou!");
 				}
-		    }
+			}
 
-		    printf("\nInsira a posição onde os misseis serão lançados!");
+			/**
+			 * Requisitando posição de envio dos misseis
+			 */
+			printf("Situação atual de sua frota:\n");
+			imprimeCampo(Tabuleiro, 0);
+			printf("Situação atual da frota de seu inimigo:\n");
+			imprimeCampo(Tabuleiro2, 0);
 
+			printf("\nInsira a posição onde os misseis serão lançados!");
 			printf("Insira a coluna da posição desejada: ");
 			scanf("%d", &Coluna);
 			while(Coluna < 0 || Coluna > 9){
 				printf("\nColuna fora dos limites do tabuleiro, favor inserir uma entre 0 e 9.");
 				scanf("%d", &Coluna);
 			}
-
 			printf("\nInsira a linha da posição desejada: ");
 			scanf("%d", &Linha);
 			while(Linha < 0 || Linha > 9){
@@ -132,67 +210,84 @@ int main(){
 				scanf("%d", &Linha);
 			}
 
+			/**
+			 * Se existe resposta de tiro acrescenta info no final da msg
+			 */
 			if(shot>=0) sprintf(msg, "s%d%dr%d",Coluna, Linha, shot);
 			else sprintf(msg, "s%d%d",Coluna, Linha);
 
-		    n = sendto(servSock, msg, strlen(msg), 0, (struct sockaddr *)&cliAddr, sizeof(cliAddr));
+			/**
+			 * Resposta para o cliente com os dados na mensagem
+			 */
+			n = sendto(servSock, msg, strlen(msg), 0, (struct sockaddr *)&cliAddr, sizeof(cliAddr));
 
-		    if(n<0){
-		    	printf("Could't send back data");
-		    	continue;
-		    }
-
-
-
-		  }/* end of server infinite loop */
+			if(n<0){
+				printf("Could't send back data");
+				continue;
+			}
+		}
+		/**
+		 * Fim do servidor
+		 */
 
     }
-    else if(kind == 2){//client stuff
-
-    	h = gethostbyname("127.0.0.1");
-		  if(h==NULL) {
-		    printf("%s: unknown host '%s' \n", "argv[0]", "argv[1]");
-		//    exit(1);
-		  }
-
-		  printf("%s: sending data to '%s' (IP : %s) \n", "argv[0]", h->h_name,
-			 inet_ntoa(*(struct in_addr *)h->h_addr_list[0]));
-
-		  remoteServAddr.sin_family = h->h_addrtype;
-		  memcpy((char *) &remoteServAddr.sin_addr.s_addr,
-			 h->h_addr_list[0], h->h_length);
-		  remoteServAddr.sin_port = htons(REMOTE_SERVER_PORT);
-
-		  /* socket creation */
-		  sd = socket(AF_INET,SOCK_DGRAM,0);
-		  if(sd<0) {
-		    printf("%s: cannot open socket \n","argv[0]");
-		//    exit(1);
-		  }
-
-		  /* bind any port */
-		  cliAddr.sin_family = AF_INET;
-		  cliAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-		  cliAddr.sin_port = htons(0);
-
-		  rc = bind(sd, (struct sockaddr *) &cliAddr, sizeof(cliAddr));
-		  if(rc<0) {
-		    printf("%s: cannot bind port\n", "argv[0]");
-		  //  exit(1);
-		  }
+    else if(kind == 2){
+		/**
+		* Trabalhando como client
+		*/
+		/**
+		* Estabelece o host, ja tras o struct addr
+		*/
+		h = gethostbyname("127.0.0.1");
+		if(h==NULL) {
+			printf("%s: unknown host '%s' \n", "argv[0]", "argv[1]");
+			//    exit(1);
+		}
 
 
+		printf("Enviando dados para: '%s' (IP : %s) \n", h->h_name,
+		inet_ntoa(*(struct in_addr *)h->h_addr_list[0]));
 
+		/**
+		 * Monta o endereço do servidor pela respota do gothostbyname
+		 */
+		remoteServAddr.sin_family = h->h_addrtype;
+		memcpy((char *) &remoteServAddr.sin_addr.s_addr,
+		h->h_addr_list[0], h->h_length);
+		remoteServAddr.sin_port = htons(REMOTE_SERVER_PORT);
 
-		  /**
-		   * OLHA AQUI SEUS FILHO DE QUENGA
-		   * COMO ESSE Ë A PORRA DO CLIENTE ELE ENVIA E DEPOIS RECEBE
-		   * NÃO SEI SE ESSA ORDEM INFLUENCIA MAS PRA MIM FAZ SENTIDO ASSSIM
-		   */
+		/**
+		 * Cria o socket do cliente
+		 */
+		sd = socket(AF_INET,SOCK_DGRAM,0);
+		if(sd<0) {
+			printf("%s: cannot open socket \n","argv[0]");
+			//    exit(1);
+		}
 
-		  /* send data */
-		  for(i = 0;;i++) {
-		  	printf("Situação atual de sua frota:\n");
+		/**
+		 * Cria o endereço do cliente, e o vincula com qualque porta
+		 * disponível?
+		 */
+		cliAddr.sin_family = AF_INET;
+		cliAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+		cliAddr.sin_port = htons(0);
+
+		rc = bind(sd, (struct sockaddr *) &cliAddr, sizeof(cliAddr));
+		if(rc<0) {
+			printf("%s: cannot bind port\n", "argv[0]");
+			//  exit(1);
+		}
+
+		/**
+		 * Loop infinito estranho, mas eu precisava do i pra alguma coisa
+		 */
+		for(i = 0;;i++) {
+
+			/**
+			 * Requisitando posição de envio dos misseis
+			 */
+			printf("Situação atual de sua frota:\n");
 			imprimeCampo(Tabuleiro, 0);
 			printf("Situação atual da frota de seu inimigo:\n");
 			imprimeCampo(Tabuleiro2, 0);
@@ -211,52 +306,64 @@ int main(){
 				scanf("%d", &Linha);
 			}
 
+			/**
+			 * Se existe resposta de tiro acrescenta info no final da msg
+			 */
 			if(shot>=0) sprintf(msg, "s%d%dr%d",Coluna, Linha, shot);
 			else sprintf(msg, "s%d%d",Coluna, Linha);
 
-		    rc = sendto(sd, msg, strlen(msg)+1, 0,
-				(struct sockaddr *) &remoteServAddr,
-				sizeof(remoteServAddr));
+			rc = sendto(sd, msg, strlen(msg)+1, 0,
+			(struct sockaddr *) &remoteServAddr,
+			sizeof(remoteServAddr));
 
-		    if(rc<0) {
-		      printf("%s: cannot send data %d \n","argv[0]",i-1);
-		      close(sd);
-		  //    exit(1);
-		    }
+			if(rc<0) {
+				printf("%s: cannot send data %d \n","argv[0]",i-1);
+				close(sd);
+				//    exit(1);
+			}
 
-		    /**
-		     * TOU USANDO A CARALHA DO cliLen MAS Ë O TAMANHO DO ENDERECO
-		     * DO FILHADAPUTA DO SERVIDOR COMO TA NO MESMO ARQUIVO ACHO
-		     * QUE DARIA PRA USAR A MESMA VARIÁVEL QUE O SERVADDR
-		     * MAS NAO SE MECHE EM TIME QUE TA GANHANDO
-		     */
-		    cliLen = sizeof(remoteServAddr);
-		    rc = recvfrom(sd, msg, MAX_MSG, 0, (struct sockaddr *) &remoteServAddr, &cliLen);
+			/**
+			 * Aqui ele vai enviar os dados para o server
+			 * Salvo o tamanho do cliente em uma variavel e passo o endereço dela,
+			 * provavelmente o recvfrom faz a mágica de calcular o tamanho de
+			 * retorno do endereço
+			 */
+			cliLen = sizeof(remoteServAddr);
+			rc = recvfrom(sd, msg, MAX_MSG, 0, (struct sockaddr *) &remoteServAddr, &cliLen);
 
-		    if(msg[0] == 's'){
-		    	shot = tiro(Tabuleiro, msg[1], msg[2]);
-		    }
+			if(msg[0] == 's'){
+				shot = tiro(Tabuleiro, msg[1], msg[2]);
+			}
 
-		  if(strlen(msg)>3 && msg[3] == 'r'){
-			    if(msg[4] == '1'){ //Caso a linha e a coluna batam com a # no tabuleiro verdadeiro do jogador 2, sera efetuado e mostrado o X em ambos os tabuleiros
-					Tabuleiro2[Linha][Coluna].c = 'X';
-					printf("\nVoce acertou!\n");
-					Acertos++; //o numero de acertos sobe
+			if(strlen(msg)>3 && msg[3] == 'r'){
+				/**
+				 * Caso a linha e a coluna batam com a # no tabuleiro verdadeiro
+				 * do jogador 2, sera efetuado e mostrado o X em ambos os
+				 * tabuleiros
+				 */
+				if(msg[4] == '1'){
+				Tabuleiro2[Linha][Coluna].c = 'X';
+				printf("\nVoce acertou!\n");
+				Acertos++; //o numero de acertos sobe
 				}
 				else if(msg[4] == '2'){
-					printf("\nVoce ja atirou nessa Posicao anteriormente.\n");
+				printf("\nVoce ja atirou nessa Posicao anteriormente.\n");
 				}
-				else{ //Caso contrario sera mostrado um "." no tabuleiro de visao do jogador 1 e um ! no tabuleiro verdadeiro do jogador 2
-					Tabuleiro2[Linha][Coluna].c = '.';
-					printf("\nVoce errou!");
+				/**
+				 * Caso contrario sera mostrado um "." no tabuleiro de visao
+				 * do jogador 1 e um ! no tabuleiro verdadeiro do jogador 2
+				 */
+				else{
+				Tabuleiro2[Linha][Coluna].c = '.';
+				printf("\nVoce errou!");
 				}
-		    }
+			}
 
-		    printf("%s: from %s:UDP%u : %s size: %d\n",
-			   "argv[0]",inet_ntoa(remoteServAddr.sin_addr),
-			   ntohs(remoteServAddr.sin_port),msg, rc);
+			// printf("%s: from %s:UDP%u : %s size: %d\n",
+			// "argv[0]",inet_ntoa(remoteServAddr.sin_addr),
+			// ntohs(remoteServAddr.sin_port),msg, rc);
 
-		  }
+		}
 
     }
     else{
